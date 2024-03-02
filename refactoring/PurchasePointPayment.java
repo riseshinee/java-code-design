05c93c79691510e1e13b7777f60afec9bc6c96d4
seungheee <lsh3431@gmail.com>
2024-03-02 23:42:13 +0900

    feat: refactoring
    - 조건을 읽기 쉽게 하기

---
+++ refactoring/PurchasePointPayment.java
@@ -0,0 +1,17 @@
+package refactoring;
+
+/**
+ * 조건을 읽기 쉽게 하기
+ * 예시: 포인트로 구매가 가능한지 체크하는 클래스
+ */
+public class PurchasePointPayment {
+  PurchasePointPayment(final Customer customer, final Comic comin) {
+    //!customer.isEnabled() 보다는 customer.isDisabled() 읽기 쉬움
+    if (customer.isDisabled()) {
+      throw new IllegalArgumentException("유효하지 않은 계정입니다.");
+    }
+    if (comic.isDisabled()) {
+      throw new IllegalArgumentException("현재 구매할 수 없는 만화입니다.");
+    }
+  }
+}
